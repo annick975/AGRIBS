f4e80ddf820fbb1ce7afdfe7e6b4909530c26658
annick975 <niyubahwe.annick975@gmail.com>
2024-09-12 06:47:56 +0300

    Slight changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header/header";
+import Home from "./components/Home";
+import About from "./components/About/about";
 function App() {
   return (
     <>
       <header>
         <Header />
       </header>
+      <Home />
+      <About />
     </>
   );
 }
---
+++ client/src/components/Home/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./home.css"
+
+const Home: React.FC = () => {
+  return (
+    <div className="landing-page">
+      <div className="content">
+        <h1 className="main-title">
+          The <span className="highlight">Solution</span> To Your Market
+        </h1>
+        <h2 className="subtitle">Access Needs</h2>
+        <p className="description">
+          Easily Connect with Buyers Through AGRIBS, Maximize Your Profits and
+          Minimize Market Challenges
+        </p>
+        <button className="cta-button">Join Our Community</button>
+      </div>
+    </div>
+  );
+};
+export default Home;
